424fac22cf4d9d3ea6cfe27c545f011b8b6c4698
Lucas Noronha <lucasnoronha7777@gmail.com>
2022-06-10 20:14:57 -0300

    Add server class

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket 
 import sys
 from time import sleep
-from server import Request
+from models import Request
 
 HOST = 'localhost'
 PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
@@ -14,12 +14,11 @@ print('Connected')
 
 request = Request(
     path='/',
-    method='POST'
+    method='POST',
+    headers=['Keep-Alive: timeout=10']
 )
 
-request.build()
-
-message = request.request
+message =request.build()
 
 print(message)
 
@@ -29,17 +28,20 @@ response = s.recv(1024).decode('ascii')
 
 print(response)
 
-# sleep(2)
+sleep(2)
 
-# print('again')
-# sleep(1)
+print('again')
+sleep(1)
 
-# message = 'GET / HTTP/1.1\r\n\r\n'
+message = Request(
+    path='/',
+    method='POST'
+).build()
 
-# s.send(message.encode('ascii'))
+s.send(message.encode('ascii'))
 
-# response = s.recv(1024).decode('ascii')
+response = s.recv(1024).decode('ascii')
 
-# print(response)
+print(response)
 
-# sleep(10)
+sleep(7)
--- server.py
+++ server.py
@@ -1,122 +1,126 @@
 import socket
 import threading
-import sys
 from time import sleep
-from configs import HOST, PORT
+
 from models import Request, Response
 from exceptions import ServerException
+from configs import HOST, PORT
 
-PORT = int(sys.argv[1]) if len(sys.argv) > 1 else 8080
-
-def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    s.bind((HOST, PORT))
-
-    s.listen()
-
-    print(f'listenning on port {PORT}')
-
-    while True:
-        client, address = s.accept()
-
-        print('connection accepted')
-
-        tr = threading.Thread(target=handleConnection, args=(client, address))
-        tr.daemon = True
-        tr.start()
+class Server:
+    def __init__(self, host=HOST, port=PORT):
+        self.host = host
+        self.port = port
 
 
-def handleConnection(client, address):
-    message = client.recv(1024).decode('ascii')
-    timeout = checkForPersistentConnection(message)
-    if timeout == None:
-        handleRequest(client, address, message)
-        client.close()
-        print('connection closed')
-    else:
-        # TODO: improve this strategy using semaphores
-        # Using the shared variable connection status may cause race conditions
-        # Using connectionStatus as list because boolean are passed by value in python
-        connectionStatus = [True]
-        tr = threading.Thread(target=connectionTimeoutCounter, args=(client, timeout, connectionStatus))
-        tr.daemon = True
-        tr.start()
-        while connectionStatus[0]:
-            handleRequest(client, address, message)
-            message = client.recv(1024).decode('ascii')
+    def start(self):
+        print('Starting server...')
+        
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((self.host, self.port))
+        s.listen()
+
+        print(f'Listenning on port {self.port}')
+
+        while True:
+            client, address = s.accept()
+
+            clientHost, clientPort = client.getpeername()
+
+            print(f'Connection accepted: {clientHost}:{clientPort}')
+
+            tr = threading.Thread(target=self.handleConnection, args=(client,))
+            tr.daemon = True
+            tr.start()
+
+
+    def handleConnection(self, client):
+        message = client.recv(1024).decode('ascii')
+        timeout = self.checkForPersistentConnection(message)
+
+        if timeout == None:
+            self.handleRequest(client, message)
+            clientHost, clientPort = client.getpeername()
+            client.close()
+            print(f'connection closed: {clientHost}:{clientPort}')
+        else:
+            # TODO: improve this strategy using semaphores
+            # Using the shared variable connection status may cause race conditions
+            # Using connectionStatus as list because boolean are passed by value in python
+            connectionStatus = [True]
+            tr = threading.Thread(target=self.connectionTimeoutCounter, args=(client, timeout, connectionStatus))
+            tr.daemon = True
+            tr.start()
+            while connectionStatus[0]:
+                self.handleRequest(client, message)
+                message = client.recv(1024).decode('ascii')
         
 
-def connectionTimeoutCounter(client, timeout, connectionStatus):
-    sleep(timeout)
-    connectionStatus[0] = False
-    client.close()
-    print('connection closed')
+    def handleRequest(self, client, message):
+        try:
+            request = Request(message)
 
-def handleRequest(client, address, message):
-    try:
-        request = Request(message)
+            if request.method == 'GET':
+                response = self.handleGetRequest(request.path)
+                client.send(response.encode('ascii'))
 
-        print(request.path)
-        print(request.method)
-        print(request.headers)
-        print(request.body)
+            elif request.method == 'POST':
+                response = self.handlePostRequest(request.path, request.body)
+                client.send(response.encode('ascii'))
 
-        if request.method == 'GET':
-            response = handleGetRequest(request.path)
+        except ServerException as ex:
+            response = Response(ex.status, ex.statusMessage, ex.message).build()
             client.send(response.encode('ascii'))
-        elif request.method == 'POST':
-            response = handlePostRequest(request.path, request.body)
-            client.send(response.encode('ascii'))
-    except ServerException as ex:
-        response = Response(ex.status, ex.statusMessage, ex.message).build()
-        client.send(response.encode('ascii'))
 
 
-def handleGetRequest(path):
-    if path == '/':
-        path = '/index.html'
+    def handleGetRequest(self, path):
+        if path == '/':
+            path = '/index.html'
 
-    fullPath = './static' + path
+        fullPath = './static' + path
 
-    try:
-        f = open(fullPath, 'r')
-        fileContent = f.read()
-        f.close()
-        return Response(status=200, statusMessage='OK', message=fileContent).build()
-    except:
-        raise ServerException(404, 'Not Found')
-
-def handlePostRequest(path, body):
-
-    if path == '/':
-        path = '/index.html'
-
-    fullPath = './static' + path
-
-    try:
-        f = open(fullPath, 'r')
-        fileContent = f.read()
-        f.close()
-        fileContent = fileContent.split('<body>')
-        responseMessage = fileContent[0] + '<body>\n' + body + fileContent[1]
-        return Response(status=200, statusMessage='OK', message=responseMessage).build()
-    except:
-        raise ServerException(404, 'Not Found')
+        try:
+            f = open(fullPath, 'r')
+            fileContent = f.read()
+            f.close()
+            return Response(status=200, statusMessage='OK', message=fileContent).build()
+        except:
+            raise ServerException(404, 'Not Found')
 
 
-def checkForPersistentConnection(message):
-    lines = message.split('\r\n')
-    for line in lines:
-        # TODO: treat cases where the keep-alives fields are wrong
-        if line.find('Keep-Alive') != -1:
-            value = line.split(':')[1].strip()
-            fields = value.split(',')
-            for field in fields:
-                if field.find('timeout') != -1:
-                    return int(field.split('=')[1])
-    return None
+    def handlePostRequest(self, path, body):
+        if path == '/':
+            path = '/index.html'
+
+        fullPath = './static' + path
+
+        try:
+            f = open(fullPath, 'r')
+            fileContent = f.read()
+            f.close()
+            fileContent = fileContent.split('<body>')
+            responseMessage = fileContent[0] + '<body>\n' + body + fileContent[1]
+            return Response(status=200, statusMessage='OK', message=responseMessage).build()
+        except:
+            raise ServerException(404, 'Not Found')
 
 
-if __name__ == '__main__':
-    main()
+    def connectionTimeoutCounter(self, client, timeout, connectionStatus):
+        sleep(timeout)
+        clientHost, clientPort = client.getpeername()
+        connectionStatus[0] = False
+        client.close()
+        print(f'connection closed: {clientHost}:{clientPort}')
+        
+
+
+    def checkForPersistentConnection(self, message):
+        lines = message.split('\r\n')
+        for line in lines:
+            # TODO: treat cases where the keep-alives fields are wrong
+            if line.find('Keep-Alive') != -1:
+                value = line.split(':')[1].strip()
+                fields = value.split(',')
+                for field in fields:
+                    if field.find('timeout') != -1:
+                        return int(field.split('=')[1])
+        return None
